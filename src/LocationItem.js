ec4a78e13993cdf68890429b1c022e8b476d3c77
Arthur Benware <artpolitic91@gmail.com>
2023-02-21 13:34:06 -0500

    cleaned up code

--- src/LocationItem.js
+++ src/LocationItem.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function LocationItem({ location }) {
+    const { id, name, image, text} = location
+
     const [basedCount, setBasedCount] = useState(0);
     const [midCount, setMidCount] = useState(0);
     const [cringeCount, setCringeCount] = useState(0);
@@ -18,12 +20,12 @@ function LocationItem({ location }) {
     }    
     return (
         <div>
-            <h1>{location.name}</h1>
-            <img alt={location.id} src={location.image} />
+            <h1>{name}</h1>
+            <img alt={id} src={image} />
             <button onClick={handleBasedCountChange}>Based {basedCount}</button>
             <button onClick={handleMidCountChange}>Mid {midCount}</button>
             <button onClick={handleCringeCountChange}>Cringe {cringeCount}</button>
-            <p className="location_bio">{location.text}</p>
+            <p className="location_bio">{text}</p>
         </div>
     )
 }
